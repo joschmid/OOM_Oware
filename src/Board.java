e3fcbfb91e5a722c465f3a92661376d8cf35001e
Johannes Schmid <JohannesS92@web.de>
2016-05-25 16:09:48 +0200

    Kommentare eingefügt

--- src/Board.java
+++ src/Board.java
@@ -3,7 +3,13 @@ public class Board {
 	int[] muldenUnten = new int[6];
 	int[] muldenOben = new int[6];
 
-	public Board() {
+	public Spieler sp1;
+	public Spieler sp2;
+
+	public Board(Spieler sp1, Spieler sp2) {
+
+		this.sp1 = sp1;
+		this.sp2 = sp2;
 
 		for (int k = 0; k < muldenOben.length; k++) {
 			muldenOben[k] = 4;
@@ -14,9 +20,29 @@ public class Board {
 
 	}
 
-	public String toString() {
+	public Board() {
+		this.sp1 = null;
+		this.sp2 = null;
 
-		return "-------------------------------------------------------------------";
+		for (int k = 0; k < muldenOben.length; k++) {
+			muldenOben[k] = 4;
+		}
+		for (int k = 0; k < muldenUnten.length; k++) {
+			muldenUnten[k] = 4;
+		}
+	}
+
+	void boardAnzeigen() {
+
+		System.out.print("================="+sp2.name+"==================\n");
+		System.out.print("A\tB\tC\tD\tE\tF\n");
+		System.out.print(muldenOben[0]+"\t"+muldenOben[1]+"\t"+muldenOben[2]+"\t"+muldenOben[3]+"\t"+muldenOben[4]+"\t"+muldenOben[5]+"\n");
+		System.out.print("-------------------------------------------\n");
+		System.out.print(muldenUnten[0]+"\t"+muldenUnten[1]+"\t"+muldenUnten[2]+"\t"+muldenUnten[3]+"\t"+muldenUnten[4]+"\t"+muldenUnten[5]+"\n");
+		System.out.print("a\tb\tc\td\te\tf\n");
+		System.out.print("===================="+sp1.name+"====================\n");
+		System.out.print(sp2.name+" Score: "+sp2.depot+"\n"+sp1.name+ " Score: "+sp1.depot+"\n\n");
+
 
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,26 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 
+		// Neues Board erstellen
 		Board board = new Board();
 
+		// Kommandozeile einlesen
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		System.out.println("Wollen sie gegen einen menschlichen Spieler spielen? (j/n)");
 		String eingabe1 = br.readLine();
 
+		// Benutzer, Spieler, Computer anlegen
 		User sp1 = new User();
 		Spieler sp2 = new User();
+		Computer spc = new Computer();
 
+		// Ob gegen einen Computer gespielt wird
+		boolean spctrue = false;
+
+		// Spielstart
 		switch (eingabe1) {
+		// Zwei menschliche Spieler
 		case "j":
 			System.out.println("Spieler 1 Name eingeben:");
 			String name1 = br.readLine();
@@ -28,6 +37,7 @@ public class Main {
 			sp2.name = name2;
 			break;
 
+		// Gegen Computer spielen
 		case "n":
 			System.out.println("Spieler 1 Name eingeben:");
 			String name3 = br.readLine();
@@ -36,27 +46,121 @@ public class Main {
 			System.out.println("Bitte Schwierigkeit eingeben(leicht/mittel/schwer)");
 			String schwierigkeit = br.readLine();
 
-			Computer spc = new Computer();
-			boolean spctrue = true;
+			spctrue = true;
+			// Schwierigkeit festlegen
 			switch (schwierigkeit) {
 			case "leicht":
 				spc.schwierigkeitsgrad = schwierigkeit;
+				spc.name = "Computer (leicht)";
 				break;
 
 			case "mittel":
 				spc.schwierigkeitsgrad = schwierigkeit;
-
+				spc.name = "Computer (mittel)";
 				break;
 
 			case "schwer":
 				spc.schwierigkeitsgrad = schwierigkeit;
+				spc.name = "Computer (schwer)";
 				break;
 			default:
-				System.out.println("Fehler");
+				System.out.println("Falsche Eingabe! Bitte Spiel neustarten");
 			}
 			break;
 		default:
-			System.out.println("Fehler2");
+			System.out.println("Falsche Eingabe! Bitte Spiel neustarten");
+		}
+
+		// Festlegen welcher Spieler beginnt
+		boolean anDerReihe = sp1Beginnt();
+
+		if (anDerReihe) {
+			System.out.println(sp1.name + " beginnt");
+		} else {
+			if (spctrue) {
+				System.out.println(spc.name + " beginnt");
+			} else {
+				System.out.println(sp2.name + " beginnt");
+			}
+		}
+
+		// Board die spielenden Spieler bekannt machen
+		board.sp1 = sp1;
+		if (spctrue) {
+			board.sp2 = spc;
+		} else {
+			board.sp2 = sp2;
+		}
+
+		// do {
+		// board.boardAnzeigen();
+		// if (spctrue) {
+		// // Computer Spielzug implementieren
+		// } else {
+		// System.out.printf("Your Move: ");
+		// String a = br.readLine();
+		// x = a.charAt(0);
+		//
+		// switch (x) {
+		// case 'a':
+		// result = game.makeMove(0);
+		// break;
+		//
+		// case 'b':
+		// result = game.makeMove(1);
+		// break;
+		//
+		// case 'c':
+		// result = game.makeMove(2);
+		// break;
+		//
+		// case 'd':
+		// result = game.makeMove(3);
+		// break;
+		//
+		// case 'e':
+		// result = game.makeMove(4);
+		// break;
+		//
+		// case 'f':
+		// result = game.makeMove(5);
+		// break;
+		//
+		// case 'x':
+		// case 'X':
+		// for (int i = 0; i < 6; i++) {
+		// game.computerSeeds += game.computerCells[i];
+		// game.playerSeeds += game.playerCells[i];
+		// }
+		// if (game.computerSeeds == 24 && game.playerSeeds == 24) {
+		// System.out.println("It is a draw!");
+		// }
+		// if (game.computerSeeds > 24) {
+		// System.out.println("Computer Won!");
+		// }
+		// if (game.playerSeeds > 24) {
+		// System.out.println("You won!");
+		// }
+		//
+		// break;
+		//
+		// default:
+		// System.out.print("Invalid Move\n");
+		// break;
+		// }
+		// }
+		//
+		// } while (x != 'x' && x != 'X' && result != 1);
+		//
+		//
+	}
+
+	public static boolean sp1Beginnt() {
+		double i = Math.random();
+		if (i <= 0.5) {
+			return true;
+		} else {
+			return false;
 		}
 
 	}
